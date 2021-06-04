c2dcd3d05b0f03b106053ad2b5c20a9d7abc1e51
bhavani125 <girinenibhavani12@gmil.com>
2021-06-04 13:33:03 +0530

    percentage of head and  tail

--- Flipcoincombination.sh
+++ Flipcoincombination.sh
@@ -1,11 +1,21 @@
 heads=1
 tails=0
+declare -A res
 
-toss=$((RANDOM%2))
+echo "Enter n for the loop "
+read n
+for (( i=0;i<n;i++ ))
+do
+   toss=$((RANDOM%2))
+   if [ $toss -eq $heads ]
+   then
+       res['heads']=$(("${res['heads']}"+1))
+   else
+       res['tails']=$(("${res['tails']}"+1))
 
-if [ $toss -eq $heads ]
-then
-   echo "Heads win"
-else
-   echo "Tails win "
-fi
+   fi
+done
+headcnt=$(($n-${res['heads']}))
+headpercent=$((($headcnt*100)/$n))
+echo "tails=${res['tails']} and heads=${res['heads']}"
+echo "occurance of tails = $headpercent% and head = $((100-$headpercent))%"
